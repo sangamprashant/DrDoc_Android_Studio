d95ad69c21848a96133613fdce4df67c1068903f
Prashant Srivastav <srivastavp891@gmail.com>
2023-07-02 12:09:36 +0530

    11 commit Profile/About/switch b/t acc

--- app/src/main/java/com/sangamprashant/drdoc/SettingsFragment.java
+++ app/src/main/java/com/sangamprashant/drdoc/SettingsFragment.java
@@ -13,6 +13,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -33,8 +35,16 @@ public class SettingsFragment extends Fragment {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
+                if (item.getItemId() == R.id.nav_profile) {
+                    showProfileDialog();
+                    return true;
+                }
+                if (item.getItemId() == R.id.nav_change_account) {
+                    showChangeAccountDialog();
+                    return true;
+                }
                 if (item.getItemId() == R.id.nav_logout) {
-                    showBottomDialog();
+                    showLogoutDialog();
                     return true;
                 }
                 if (item.getItemId() == R.id.nav_about) {
@@ -48,7 +58,92 @@ public class SettingsFragment extends Fragment {
         return view;
     }
 
-    private void showBottomDialog() {
+    private void showProfileDialog() {
+        final Dialog dialog = new Dialog(requireContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.profile_photo);
+
+        ImageButton ImageToSelect = dialog.findViewById(R.id.ImageToUpload);
+        Button UploadSelectedImage = dialog.findViewById(R.id.uploadSelectedImage);
+        Button RemoveCurrentPhoto = dialog.findViewById(R.id.removeCurrentProfile);
+        Button CanceledUpload = dialog.findViewById(R.id.canceledProfileUpload);
+        ImageToSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(requireActivity(),"ImageToSelect is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        UploadSelectedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Toast.makeText(requireActivity(),"UploadSelectedImage is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        RemoveCurrentPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Toast.makeText(requireActivity(),"RemoveCurrentPhoto is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        CanceledUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Toast.makeText(requireActivity(),"CanceledUpload is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.CENTER);
+    }
+    private void showChangeAccountDialog () {
+        final Dialog dialog = new Dialog(requireContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.change_account_type);
+
+        LinearLayout RegularAccount = dialog.findViewById(R.id.regularAccount);
+        LinearLayout DoctorAccount = dialog.findViewById(R.id.doctorAccount);
+        LinearLayout SellerAccount = dialog.findViewById(R.id.sellerAccount);
+        Button CanceledAccountChange = dialog.findViewById(R.id.cancelAccountChange);
+
+        RegularAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(requireActivity(),"RegularAccount is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        DoctorAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(requireActivity(),"DoctorAccount is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        SellerAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(requireActivity(),"SellerAccount is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        CanceledAccountChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Toast.makeText(requireActivity(),"CanceledAccountChange is clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.CENTER);
+    }
+    private void showLogoutDialog() {
         final Dialog dialog = new Dialog(requireContext());
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.bottomsheetlayout);
